3939b64231eff9fe3f8bbd530d5c03eff4243ef5
Jakub Turis <jakub@kontentino.com>
2021-12-24 11:35:39 +0100

    Cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
-import logo from './logo.svg'
+import logo from './images/logo.svg'
+import k8s from './images/k8s.png'
+import docker from './images/docker.png'
+import nginx from './images/nginx.png'
 import './App.css'
 import axios from 'axios'
 
@@ -13,7 +16,12 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+      <div className='images'>
         <img src={logo} className='App-logo' alt='logo' />
+        <img src={k8s} className='App-logo' alt='logo' />
+        <img src={docker} className='App-logo' alt='logo' />
+        <img src={nginx} className='App-logo' alt='logo' />
+      </div>
         <p>Sample React app using docker, k8s and only God knows what else</p>
         <h3 style={{ marginBottom: 20, marginTop: 0, paddingBottom: 0 }}>By</h3>
         <h4 style={{ marginBottom: 10, marginTop: 0, paddingBottom: 0 }}>
