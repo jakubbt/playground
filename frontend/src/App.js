959ac55a59c8bac77e932d0dbbc86baa6c85149e
Jakub Turis <jakub@kontentino.com>
2022-01-01 13:52:45 +0100

    Format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,25 +17,44 @@ function App() {
     axios.get('/api/version').then(res => setV(res.data))
   }, [])
 
-
   return (
     <div className='App'>
       <header className='App-header'>
-      <div className='images'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <img src={docker} className='App-logo' alt='logo' />
-        <img src={tf} className='App-logo' alt='logo' />
-        <img src={azure} className='App-logo' alt='logo' />
-        <img src={k8s} className='App-logo' alt='logo' />
-        <img src={nginx} className='App-logo' alt='logo' />
-      </div>
-        <p>Sample React app using docker, k8s, terraform and only God knows what else</p>
-        <p>Version: 1.0.{v} - try refreshing, there should be 4 pods ❤️</p>
+        <div className='images'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <img src={docker} className='App-logo' alt='logo' />
+          <img src={tf} className='App-logo' alt='logo' />
+          <img src={azure} className='App-logo' alt='logo' />
+          <img src={k8s} className='App-logo' alt='logo' />
+          <img src={nginx} className='App-logo' alt='logo' />
+        </div>
+        <p>
+          Sample React app using docker, k8s, terraform and only God knows what
+          else
+        </p>
+        <p>
+          Version: 1.0.<span style={{ color: 'green' }}>{v}</span> - try
+          refreshing, there should be 4 pods ❤️
+        </p>
         <h3 style={{ marginBottom: 20, marginTop: 0, paddingBottom: 0 }}>By</h3>
-        <h4 style={{ marginBottom: 10, marginTop: 0, paddingBottom: 0, color: 'green' }}>
+        <h4
+          style={{
+            marginBottom: 10,
+            marginTop: 0,
+            paddingBottom: 0,
+            color: 'green',
+          }}
+        >
           {me.name}
         </h4>
-        <h5 style={{ marginBottom: 10, marginTop: 0, paddingBottom: 0, color: 'green' }}>
+        <h5
+          style={{
+            marginBottom: 10,
+            marginTop: 0,
+            paddingBottom: 0,
+            color: 'green',
+          }}
+        >
           {me.bio}
         </h5>
         <code>
@@ -48,7 +67,9 @@ function App() {
             -- github --
           </a>
         </code>
-        <code style={{ fontSize: 14 }}>Green text color is from api, if there is none, sorry. 😭</code>
+        <code style={{ fontSize: 14 }}>
+          Green text color is from api, if there is none, sorry. 😭
+        </code>
       </header>
     </div>
   )
